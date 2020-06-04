3f652ffb4ca3ad410dbbba17e73ef7e9d60fe596
my <you@example.com>
2020-06-04 16:43:00 -0400

    update

--- MyLibrary/MySource/ros.bash
+++ MyLibrary/MySource/ros.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-source /home/my/Desktop/Ros_Project/demo/devel/setup.bash
 source /home/my/Desktop/Ros_Project/test_ws/devel/setup.bash
-source /home/my/Desktop/Ros_Project/hector_quadrotor/devel/setup.bash
-source /home/my/Desktop/Ros_Project/uav_project/devel/setup.bash
+source /home/my/github/ROS/demo/devel/setup.bash
+source /home/my/github/ROS/uav_project/devel/setup.bash
+source /home/my/github/ROS/uav_project/hector_quadrotor/devel/setup.bash
