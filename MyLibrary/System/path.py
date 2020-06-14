46f7e7d8f5037a2fc1e59333f5a6f30061d670fd
my <you@example.com>
2020-06-14 18:00:26 -0400

    update

---
+++ MyLibrary/System/path.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import os,sys
+file_path=os.path.abspath(__file__)
+current_path =  os.path.abspath(os.path.dirname(file_path) + os.path.sep + ".")
+upper_path = os.path.abspath(os.path.dirname(current_path) + os.path.sep + ".")
