398a9162577ff442457ebd4d38459025efb82b3f
my <you@example.com>
2020-10-19 23:23:29 -0400

    update

--- MyLibrary/MySource/ros_pkg.bash
+++ MyLibrary/MySource/ros_pkg.bash
@@ -2,4 +2,6 @@
 
 #source /home/my/Desktop/test/ros_test/devel/setup.bash
 #source /home/my/github/danial_snn_project/devel/setup.bash
-source /home/my/lab/uav_project/devel/setup.bash
+#source /home/my/lab/uav_project/devel/setup.bash
+source /home/my/Desktop/turtlebot3/devel/setup.bash
+
